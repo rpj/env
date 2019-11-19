46200402bdd71ef4a9c9c43ca0a6de03bdc16dc0
Ryan Joseph <ryan@proxy.com>
2019-11-19 15:18:11 -0800

    Add header

--- script/fp.sh
+++ script/fp.sh
@@ -4,4 +4,5 @@ if [ -z $HF ]; then
   HF='sha256'
 fi
 export FP_HASH_FUNC="$HF"
+echo $(pwd) " $0 " $(date -u +"%Y-%m-%dT%H:%M:%SZ") " '$HF'"
 find . -path '*.pub' | xargs -t -I{} -- ssh-keygen -l -E "$FP_HASH_FUNC" -f {}
